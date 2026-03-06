916e5b91880f7c053a704356ad20292f4b60e918
dtorresy <dtorresy@iesrafaelalberti.es>
2026-03-06 04:54:54 +0100

    Codigo ejercicio 3

--- src/main/kotlin/es/ies/ejercicios/u6/ej63/Ej63.kt
+++ src/main/kotlin/es/ies/ejercicios/u6/ej63/Ej63.kt
@@ -12,4 +12,14 @@ fun main() {
     println("- Completa la jerarquía y los constructores en `es.ies.ejercicios.u6.ej63`")
     println("- Añade logs en init/constructores para ver el orden de inicialización")
     println("- Actualiza este main para instanciar usando distintos constructores")
+
+    val r1 = Rectangulo(color = "rojo", etiqueta = "rect", ancho = 5, alto = 3)
+    val r2 = Rectangulo(ancho = 5, alto = 3)
+    val r3 = Rectangulo(lado = 5)
+
+    val c1 = Circulo(color = "azul", etiqueta = "circ", radio = 4)
+    val c2 = Circulo(radio = 4)
+
+    val t1 = Triangulo(base = 6, altura = 4)
+    val t2 = Triangulo(lado = 5)
 }
